545b767ca9468c6878518d5ff01ec9e3043a7859
Conor <chepenikconor@gmail.com>
2023-05-15 12:10:17 -0400

    added some text to the profile page

--- client/src/components/UserProfile.js
+++ client/src/components/UserProfile.js
@@ -3,7 +3,9 @@ import React from "react";
 const UserProfile = ({ user }) => {
     return (
     <div className="userProfile">
-        <h1>Welcome to HearSay!</h1>
+        <h1>Welcome to Habit-Chain</h1>
+        <h6>This app was built to help people stack positive habits and make it easy to visualize their success!</h6>
+        <hr />
         <p>Your Username: {user.username}</p>
         <p>Your Email: {user.email}</p>
         {user.admin ? <p>You Have Admin Privileges!</p> : null}
