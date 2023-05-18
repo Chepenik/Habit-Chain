e3d2212c757e82c12fdb81d07b6642d0be95dbae
Conor <chepenikconor@gmail.com>
2023-05-17 21:48:16 -0400

    some lite styling

--- client/src/components/HabitTile.js
+++ client/src/components/HabitTile.js
@@ -7,9 +7,15 @@ const HabitTile = (props) => {
   return (
     <div className="habit-tile">
       <Link to={`/habits/${habit.id}`} className="habit-link">
-        <p className="habit-name">{habit.name}</p>
-        <p>Reduce Friction: {habit.reduceFriction}</p>
-        <p>Why: {habit.why}</p>
+        <div className="grid-x">
+          <div className="cell small-12 medium-8">
+            <p className="habit-name">{habit.name}</p>
+          </div>
+          <div className="cell small-12 medium-4">
+            <p>Reduce Friction: {habit.reduceFriction}</p>
+            <p>Why: {habit.why}</p>
+          </div>
+        </div>
       </Link>
     </div>
   );
