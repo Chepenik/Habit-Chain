54e79a5ac3cc89a30f33e39f81d0197f1c1fc9b3
Conor <chepenikconor@gmail.com>
2023-05-27 15:20:09 -0400

    userProfile improvement

--- client/src/components/UserProfile.js
+++ client/src/components/UserProfile.js
@@ -3,10 +3,9 @@ import React from "react";
 const UserProfile = ({ user }) => {
     return (
     <div className="userProfile">
-        <h1>Welcome to Habit-Chain</h1>
+        <h1>Welcome to Habit-Chain {user.username}</h1>
         <h6>An app that uses the visual of a blockchain to stack positive habits rather than an actual blockchain to rug those who don't verify a crypto project for themselves :)</h6>
         <hr />
-        <p>Your Username: {user.username}</p>
         <p>Your Email: {user.email}</p>
         {user.admin ? <p>You Have Admin Privileges!</p> : null}
     </div>
