7fc322a91e1b01bedfa4cca60000394e5b29b61c
Conor <chepenikconor@gmail.com>
2023-05-27 22:37:30 -0400

    Made sure the whole habit was clickable

--- client/src/components/HabitTile.js
+++ client/src/components/HabitTile.js
@@ -5,20 +5,22 @@ const HabitTile = (props) => {
   const { habit, streakCount, active } = props;
 
   return (
-    <div className="habit-tile">
+    <>
       <Link to={`/habits/${habit.id}`} className="habit-link">
-        <div className="grid-x">
-          <div className="cell small-12 medium-8">
-            <p className="habit-name">{habit.name}</p>
-            <p>Streak: {streakCount}</p>
-            <p>Status: {active ? 'active' : 'inactive'}</p>
-          </div>
-          <div className="cell small-12 medium-4">
-            <img src={habit.giphy} alt="GIF" />
+        <div className="habit-tile">
+          <div className="grid-x">
+            <div className="cell small-12 medium-8">
+              <p className="habit-name">{habit.name}</p>
+              <p>Streak: {streakCount}</p>
+              <p>Status: {active ? 'active' : 'inactive'}</p>
+            </div>
+            <div className="cell small-12 medium-4">
+              <img src={habit.giphy} alt="GIF" />
+            </div>
           </div>
         </div>
       </Link>
-    </div>
+    </>
   );
 };
 
