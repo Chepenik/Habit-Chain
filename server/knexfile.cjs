d602f135c043c93f289a100ca4baf5124789ec5e
Conor <chepenikconor@gmail.com>
2023-05-28 08:35:29 -0400

    updated database to work on Heroku

--- server/knexfile.cjs
+++ server/knexfile.cjs
@@ -1,14 +1,18 @@
 const path = require("path");
-const getDatabaseUrl = require("./src/config/getDatabaseUrl.cjs");
-
-const migrationPath = "src/db/migrations";
 
 module.exports = {
-  connection: getDatabaseUrl(process.env.NODE_ENV || "development"),
+  connection: {
+    host: "ec2-44-213-228-107.compute-1.amazonaws.com",
+    user: "hmvnhklzdjqlvv",
+    password: "7c7df2e86bf30732fc3114c368cd7c36297e9be858604a34f0a7045704a815bc",
+    database: "ddlm1nm7b9ha45",
+    port: 5432,
+    ssl: { rejectUnauthorized: false } // Add this line for SSL configuration
+  },
   client: "pg",
   migrations: {
-    directory: migrationPath,
+    directory: path.join(__dirname, "src", "db", "migrations"),
     extension: "cjs",
-    stub: path.join(migrationPath, "migration.stub.cjs"),
+    stub: path.join(__dirname, "src", "db", "migrations", "migration.stub.cjs"),
   },
 };
