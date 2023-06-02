0296cdb7366a7c532ab07c1ce6ccaf501245494d
Conor <chepenikconor@gmail.com>
2023-06-01 21:59:10 -0400

    submitted something to the EE question que

--- client/src/components/registration/RegistrationForm.js
+++ client/src/components/registration/RegistrationForm.js
@@ -3,7 +3,8 @@ import ErrorList from "../layout/ErrorList";
 import FormError from "../layout/FormError";
 import config from "../../config";
 import translateServerErrors from "../../services/translateServerErrors";
-import { useHistory } from "react-router-dom";
+import { useHistory, Redirect } from "react-router-dom";
+import { GoogleLogin } from "react-google-login";
 
 const RegistrationForm = () => {
   const history = useHistory();
@@ -17,7 +18,6 @@ const RegistrationForm = () => {
 
   const [errors, setErrors] = useState({});
   const [serverErrors, setServerErrors] = useState({});
-
   const [shouldRedirect, setShouldRedirect] = useState(false);
 
   const validateInput = () => {
@@ -105,23 +105,28 @@ const RegistrationForm = () => {
     });
   };
 
-  const signUpWithGoogle = () => {
-    console.log("I have been clicked")
-    history.push("/api/v1/auth/google");
-    console.log("I have been gone through")
+  const responseGoogle = (response) => {
+    // Handle the response from Google
+    console.log(response);
   };
 
   if (shouldRedirect) {
-    location.href = "/";
+    return <Redirect to="/" />;
   }
 
   return (
     <div className="grid-container">
-      <div className="googleSignIn" >
-        <button onClick={signUpWithGoogle}>Sign up with Google</button>
+      <div className="googleSignIn">
+        <GoogleLogin
+          clientId="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" // Replace with your actual Google client ID
+          buttonText="Sign up with Google"
+          onSuccess={responseGoogle}
+          onFailure={responseGoogle} // You can handle failure if needed
+          cookiePolicy={"single_host_origin"}
+        />
       </div>
       <hr />
-        <h1>Or Register With An Email</h1>
+      <h1>Or Register With An Email</h1>
       <ErrorList errors={serverErrors} />
       <form onSubmit={onSubmit}>
         <div>
